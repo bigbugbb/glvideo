829dc750f77ac056ac59b240106762ee9e82ccb6
binbo <bigbugbb@gmail.com>
2024-02-21 13:00:20 +0800

    Update Constants.kt

--- core/src/main/java/com/binbo/glvideo/core/utils/Constants.kt
+++ core/src/main/java/com/binbo/glvideo/core/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.binbo.glvideo.core.utils
 
 object Constants {
     const val BYTES_PER_FLOAT = 4
-    const val RECORDER_INPUT_QUEUE_SIZE = 36
+    const val RECORDER_INPUT_QUEUE_SIZE = 32
 }
